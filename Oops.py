ecf98e94c176af78073cf94862754fc969a060a9
GayatriShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-03 12:16:39 +0530

    second commit

--- Oops.py
+++ Oops.py
@@ -1,1 +1,16 @@
- oops
+class computer:
+        def __init__(self,cpu,ram):
+                self.x = cpu
+                self.y = ram
+        def config(self):
+                print("config is:" , self.x, self.y)
+
+
+c = computer('i5', 19)
+d = computer('Ryzen 3',6)
+print(type(c))
+
+# computer.config(c)
+c.config()
+d.config()
+ 
