e3951613acda9090c2dc32091fd02d92d86dc933
GayatriShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-03 16:11:36 +0530

    inner class

--- Oops.py
+++ Oops.py
@@ -71,31 +71,77 @@
 # instance variable -> self keyword
 # class variable -> cls keyword
 
-class Student:
-        school = 'Tagore public school'
-        def __init__(self,a,b,c):
-                self.a = a
-                self.b = b
-                self.c = c
-        def avg(self):
-                return (self.a+ self.b+self.c)/3
-        def get_a(self):
-                return self.a
-        def set_a(self, val):
-                self.a = val
+# class Student:
+#         school = 'Tagore public school'
+#         def __init__(self,a,b,c):
+#                 self.a = a
+#                 self.b = b
+#                 self.c = c
+#         def avg(self):
+#                 return (self.a+ self.b+self.c)/3
+#         def get_a(self):
+#                 return self.a
+#         def set_a(self, val):
+#                 self.a = val
 
-        @classmethod   #decorators
-        def getSchool(cls):
-                return cls.school
+#         @classmethod   #decorators
+#         def getSchool(cls):
+#                 return cls.school
         
-        @staticmethod
-        def info():
-                print("this is student class...in abc module")
-c = Student(34,34,34)
+#         @staticmethod
+#         def info():
+#                 print("this is student class...in abc module")
+# c = Student(34,34,34)
 
-print(c.avg())
+# print(c.avg())
 
-# both can be used
-print(c.getSchool())
-print(Student.getSchool())
-print(c.info())
+# # both can be used
+# print(c.getSchool())
+# print(Student.getSchool())
+
+# print(c.info())
+
+
+#-----inner class
+
+class Student:
+        def __init__(self, name, rollno):
+                self.name = name
+                self.rollno = rollno
+
+                # the object of laptop class in inside the student class
+                self.lap = self.Laptop()
+        
+        def show(self):
+                print(self.name, self.rollno)
+                self.lap.show()
+
+
+        class Laptop:
+                def __init__(self):
+                        self.brand = 'HP'
+                        self.cpu ='i5'
+                        self.ram = 8
+                def show(self):
+                        print(self.brand, self.cpu, self.ram)
+
+
+                        
+c = Student('navin', 2)
+d = Student('jenny', 4)
+
+c.show()
+d.show()
+
+# create object of inner class inside the outer class
+lap1 = c.lap
+lap2 = d.lap
+
+print(id(lap1))
+print(id(lap2))
+
+# create object of inner class outcide the outer class provided you use outer class name to call it
+
+lap1 = Student.Laptop()
+
+print(id(lap1))
