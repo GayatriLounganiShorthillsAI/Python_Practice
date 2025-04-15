02b26748fdfb0a915ca308a7056c8ea97777c3a5
GayatriLounganiShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-15 12:16:01 +0530

    constructors in inheritence

--- Inheritance.py
+++ Inheritance.py
@@ -27,7 +27,35 @@
 # c.feature1()
 
 
+# class A:
+#        def feature1(self):
+#               print("first")
+
+#        def feature2(self):
+#               print("second")
+
+# # B is child class
+# class B:
+#        def feature3(self):
+#               print("third")
+#        def feature4(self):
+#               print("fourth")
+# class C(A,B):
+#        def feature5(self):
+#           print("fifth")    
+# a = A()
+# a.feature1()
+# a.feature2()
+
+
+# #C->B->A
+# c = C()
+# c.feature5()
+
 class A:
+       def __init__(self):
+              print("in A init")
+
        def feature1(self):
               print("first")
 
@@ -35,19 +63,14 @@ class A:
               print("second")
 
 # B is child class
-class B:
+class B(A):
+       def __init__(self):
+              print("in b init")
        def feature3(self):
               print("third")
        def feature4(self):
               print("fourth")
-class C(A,B):
-       def feature5(self):
-          print("fifth")    
-a = A()
-a.feature1()
-a.feature2()
+  
+#  GO IN INIT OF B 
+a = B() 
 
-
-#C->B->A
-c = C()
-c.feature5()
