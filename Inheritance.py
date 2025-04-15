348ffa67ad015567244110fe5871d469c627f57b
GayatriLounganiShorthillsAI <Gayatri.Loungani@shorthills.ai>
2025-04-15 12:11:11 +0530

    inheritance

---
+++ Inheritance.py
@@ -0,0 +1,53 @@
+# class A:
+#        def feature1(self):
+#               print("first")
+
+#        def feature2(self):
+#               print("second")
+
+# # B is child class
+# class B(A):
+#        def feature3(self):
+#               print("third")
+#        def feature4(self):
+#               print("fourth")
+# class C(B):
+#        def feature5(self):
+#           print("fifth")    
+# a = A()
+# a.feature1()
+# a.feature2()
+
+# b1 = B()
+# # B also inherits A 
+# b1.feature2()
+
+# #C->B->A
+# c = C()
+# c.feature1()
+
+
+class A:
+       def feature1(self):
+              print("first")
+
+       def feature2(self):
+              print("second")
+
+# B is child class
+class B:
+       def feature3(self):
+              print("third")
+       def feature4(self):
+              print("fourth")
+class C(A,B):
+       def feature5(self):
+          print("fifth")    
+a = A()
+a.feature1()
+a.feature2()
+
+
+#C->B->A
+c = C()
+c.feature5()
--- Oops.py
+++ Oops.py
@@ -1,108 +1,108 @@
-# class computer:
-#         def __init__(self,cpu,ram):
-#                 self.x = cpu
-#                 self.y = ram
-#         def config(self):
-#                 print("config is:" , self.x, self.y)
+class computer:
+        def __init__(self,cpu,ram):
+                self.x = cpu
+                self.y = ram
+        def config(self):
+                print("config is:" , self.x, self.y)
 
 
-# c = computer('i5', 19)
-# d = computer('Ryzen 3',6)
-# print(type(c))
+c = computer('i5', 19)
+d = computer('Ryzen 3',6)
+print(type(c))
 
-# # computer.config(c)
-# c.config()
-# d.config()
+# computer.config(c)
+c.config()
+d.config()
  
 
-# class Computer:
-#         def __init__(self):
-#                 self.name = "name"
-#                 self.age = 20
+class Computer:
+        def __init__(self):
+                self.name = "name"
+                self.age = 20
 
-#         def update(self):
-#                 self.age = 90
-#                 self.name = "new name"  
+        def update(self):
+                self.age = 90
+                self.name = "new name"  
 
-#         def compare(self, d):
-#                 if self.age == d.age :
-#                         return True
-#                 else:
-#                         return False
+        def compare(self, d):
+                if self.age == d.age :
+                        return True
+                else:
+                        return False
 
-# c = Computer()
-# d = Computer()
+c = Computer()
+d = Computer()
 
-# d.name = "riya"
-# d.age = 9
+d.name = "riya"
+d.age = 9
 
-# c.update()
+c.update()
 
-# if c.compare(d):
-#         print("they are same")
-# else:
-#         print("they are not same")
+if c.compare(d):
+        print("they are same")
+else:
+        print("they are not same")
 
 
-# print(id(c))
-# print(c.name, c.age)
-# print(d.name, d.age)
+print(id(c))
+print(c.name, c.age)
+print(d.name, d.age)
 
 
 # -------Types of variable
 
-# class Car:
-#  #  ----- class variable and static variables are same
-#         wheels = 4  #class variable , class namespace
-#         def __init__(self, mil, company):  #instance variable, instance namespace
-#                 self.mil=mil
-#                 self.company=company
+class Car:
+ #  ----- class variable and static variables are same
+        wheels = 4  #class variable , class namespace
+        def __init__(self, mil, company):  #instance variable, instance namespace
+                self.mil=mil
+                self.company=company
 
-# c = Car(20, "bmw")
-# d = Car(30, "range rover")
+c = Car(20, "bmw")
+d = Car(30, "range rover")
 
-# c.mil=0
-# d.company="maruti"
-# print(c.mil, c.wheels)
-# print(d.company, c.wheels, d.wheels)
+c.mil=0
+d.company="maruti"
+print(c.mil, c.wheels)
+print(d.company, c.wheels, d.wheels)
 
 
 # ---------Types of method
 # instance variable -> self keyword
 # class variable -> cls keyword
 
-# class Student:
-#         school = 'Tagore public school'
-#         def __init__(self,a,b,c):
-#                 self.a = a
-#                 self.b = b
-#                 self.c = c
-#         def avg(self):
-#                 return (self.a+ self.b+self.c)/3
-#         def get_a(self):
-#                 return self.a
-#         def set_a(self, val):
-#                 self.a = val
+class Student:
+        school = 'Tagore public school'
+        def __init__(self,a,b,c):
+                self.a = a
+                self.b = b
+                self.c = c
+        def avg(self):
+                return (self.a + self.b + self.c)/3
+        def get_a(self):
+                return self.a
+        def set_a(self, val):
+                self.a = val
 
-#         @classmethod   #decorators
-#         def getSchool(cls):
-#                 return cls.school
+        @classmethod   #decorators
+        def getSchool(cls):
+                return cls.school
         
-#         @staticmethod
-#         def info():
-#                 print("this is student class...in abc module")
-# c = Student(34,34,34)
+        @staticmethod
+        def info():
+                print("this is student class...in abc module")
+c = Student(34,34,34)
 
-# print(c.avg())
+print(c.avg())
 
-# # both can be used
-# print(c.getSchool())
-# print(Student.getSchool())
+# both can be used
+print(c.getSchool())
+print(Student.getSchool())
 
-# print(c.info())
+print(c.info())
 
 
-#-----inner class
+# -----inner class
 
 class Student:
         def __init__(self, name, rollno):
@@ -126,7 +126,7 @@ class Student:
                         print(self.brand, self.cpu, self.ram)
 
 
-                        
+
 c = Student('navin', 2)
 d = Student('jenny', 4)
 
@@ -145,3 +145,5 @@ print(id(lap2))
 lap1 = Student.Laptop()
 
 print(id(lap1))
+
+
